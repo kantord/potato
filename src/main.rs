4b2f48158839656a9d7b51cda11a213dc340d904
Daniel Kantor <git@daniel-kantor.com>
2026-03-19 21:49:08 +0100

    use unix socket

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 use axum::{Router, routing::get};
+use tokio::net::UnixListener;
 
 #[tokio::main]
 async fn main() {
+    let path = "/tmp/potato.sock";
+    let _ = std::fs::remove_file(path);
+
     let app = Router::new().route("/", get(|| async { "Hello, World!" }));
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3030").await.unwrap();
-    println!("Listening on http://localhost:3030");
+    let listener = UnixListener::bind(path).unwrap();
+    println!("Listening on {path}");
     axum::serve(listener, app).await.unwrap();
 }
