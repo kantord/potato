2b64b4cc8096aef7668213f31d66c03bebaa2119
Daniel Kantor <git@daniel-kantor.com>
2026-03-21 01:21:04 +0100

    add chat example app

---
+++ images/hello-simple/chat.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+read input
+message=$(echo "$input" | sed 's/.*"message":"\([^"]*\)".*/\1/')
+
+for word in Ok I will $message! Anything else?; do
+    echo "{\"event\":\"output\",\"data\":{\"word\":\"$word\"}}"
+    sleep 0.1
+done
+
+echo "{\"event\":\"output\",\"data\":{\"done\":true}}"
